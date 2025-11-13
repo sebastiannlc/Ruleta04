f6fe696d11ad8f440037e848f1b24235141b0513
Seba <sebastian.c2005r@gmail.com>
2025-11-12 22:41:19 -0300

    En carpetaUtilidades para crear instancias concretas de apuesta (rojo, par, etc) sin que controlador o vista deba conocerlos directamente.

--- src/main/java/Utilidades/ApuestaFactory.java
+++ src/main/java/Utilidades/ApuestaFactory.java
@@ -9,12 +9,13 @@ import Modelo.ApuestaImpar;
 public class ApuestaFactory {
 
     public static ApuestaBase crearApuesta(char etiqueta, double monto) {
+
         return switch (etiqueta) {
             case 'R' -> new ApuestaRojo(monto);
             case 'N' -> new ApuestaNegro(monto);
             case 'P' -> new ApuestaPar(monto);
             case 'I' -> new ApuestaImpar(monto);
-            default -> throw new IllegalArgumentException("Etiqueta de apuesta inválida.");
+            default -> throw new IllegalArgumentException("Etiqueta de apuesta inválida: " + etiqueta);
         };
     }
 }
